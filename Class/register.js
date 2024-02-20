6893cf3e50496f636086e8456f35dec6f372e61e
Raki270 <dario.todoroski@gmail.com>
2024-02-20 20:13:23 +0100

    End

--- Class/register.js
+++ Class/register.js
@@ -4,12 +4,33 @@ let modalTitle = document.getElementById("exampleModalLabel");
 let modalBody = document.getElementsByClassName("modal-body")[0];
 
 register.addEventListener("click", () => {
-    modalTitle.innerText = "Register";
-    modalBody.innerHTML = createRegisterForm();
-})
+  modalTitle.innerText = "Register";
+  modalBody.innerHTML = createRegisterForm();
+  let submitBtn = document.getElementById("submitBtn");
+  console.log(submitBtn);
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputArray = document.querySelectorAll("input");
+    let emptyField = false;
+    inputArray.forEach((element) => {
+      if (element.value === "") {
+        emptyField = true;
+        return;
+      }
+    });
+
+    if (emptyField) {
+      let errorAll = document.getElementById("errorAll");
+      errorAll.innerText = "Please fill all of the fields";
+      return;
+    }
+    postForm();
+  });
+});
 
 function createRegisterForm() {
-    return `
+  return `
     <form class="row g-3 needs-validation dis-flex_justcont-cent width-100" novalidate id="myForm">
     <div class="error" id="errorAll"></div>  
     <div class="col-md-7">
@@ -55,3 +76,39 @@ function createRegisterForm() {
     </form>
     `
 };
+
+function postForm() {
+  let firstname = document.getElementById("firstName").value;
+  let lastname = document.getElementById("lastName").value;
+  let city = document.getElementById("city").value;
+  let email = document.getElementById("email").value;
+  let job = document.getElementById("job").value;
+  let password = document.getElementById("password").value;
+
+  const user = {
+    firstName: firstname,
+    lastName: lastname,
+    city: city,
+    email: email,
+    job: job,
+    password: password,
+  };
+
+  console.log(user);
+
+  const postData = JSON.stringify(user);
+
+  fetch("https://65d38018522627d50109056a.mockapi.io/api/users", {
+    method: "POST",
+    body: postData,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => {
+    if (response.ok) {
+      let form = document.getElementById("myForm");
+      form.reset();
+    }
+    return response.json();
+  });
+}
